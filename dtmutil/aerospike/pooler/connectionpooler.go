856478192d172955826cbb2b8f4d143600aa8e23
Oscar M Herrera <oscar@kss-inc.com>
2022-07-13 14:39:35 +0200

    Cleaned up the connection pool code, removed roundrobin server.  Also added the code to ensure that a client when requested is fully formed, and all sub go routines are complete.

--- dtmutil/aerospike/pooler/connectionpooler.go
+++ dtmutil/aerospike/pooler/connectionpooler.go
@@ -1,17 +1,13 @@
 package pooler
 
 import (
-	"errors"
 	as "github.com/aerospike/aerospike-client-go/v5"
 	"github.com/dtm-labs/dtm/dtmcli/dtmimp"
 	"github.com/dtm-labs/dtm/dtmcli/logger"
 	"github.com/silenceper/pool"
-	"strconv"
-	"strings"
+	"sync"
 	"time"
 )
-
-//var authVars *utils.ASAuthentication
 
 var connectionPool *ASConnectionPool
 
@@ -46,48 +42,28 @@ func InitializeConnectionPool(config *AerospikePoolConfig) (*ASConnectionPool, e
 	var asServer []string
 	asServer = append(asServer, seedServer)
 
-	node, err := ConvertIPStringToIpPort(&asServer)
+	node, err := convertStringToIPNode(seedServer)
 	dtmimp.E2P(err)
 
-	c, err := as.NewClient(node[0].IP.String(), node[0].Port)
-	dtmimp.E2P(err)
-
-	nodes := c.Cluster().GetNodes()
-	asServers := make([]*string, len(nodes))
-	for i, v := range nodes {
-
-		host := v.GetHost().String()
-		asServers[i] = &host
-	}
-
-	roundRobinASServers, err := NewRoundRobin(asServers...)
 	if err != nil {
 		logger.Errorf(err.Error())
 		return nil, err
 	}
 	factory := func() (interface{}, error) {
-		nextServer := *roundRobinASServers.Next()
-		parts := strings.Split(nextServer, ":")
-		if len(parts) <= 1 {
-			logger.FatalIfError(errors.New("invalid server address it shoud be <ip:port>"))
-		}
-		port, err := strconv.Atoi(parts[1])
-		if err != nil {
-			logger.FatalIfError(errors.New("invalid port provide it shoud be <ip:port>"))
-		}
+
+		policy := as.NewClientPolicy()
+		policy.MinConnectionsPerNode = 1
+		policy.Timeout = time.Duration(60 * time.Second)
+
 		if UseAerospikeAuth {
 
 			//authVars = config.ASAuth
-			policy := as.NewClientPolicy()
-			policy.MinConnectionsPerNode = 50
 			policy.User = config.User
 			policy.Password = config.Password
-			policy.Timeout = time.Duration(60 * time.Second)
-			logger.Debugf("connection factory: new connection with policy to server(%s) port(%d)", parts[0], port)
-			return as.NewClientWithPolicy(policy, parts[0], port)
+			logger.Debugf("connection factory: new connection with policy to server(%s) port(%d)", node.IP.String(), node.Port)
+			return as.NewClientWithPolicy(policy, node.IP.String(), node.Port)
 		}
-		//logger.Debugf("connection factory: new connection with default policy to server(%s) port(%d)", parts[0], port)
-		return as.NewClient(parts[0], port)
+		return as.NewClientWithPolicy(policy, node.IP.String(), node.Port)
 	}
 
 	//closeConn
@@ -121,19 +97,57 @@ func getConnectionPool() *ASConnectionPool {
 	return connectionPool
 }
 
-func (p *ASConnectionPool) Get() (interface{}, error) {
-	conn, err := p.connPool.Get()
-	//logger.Debugf("connection get, pool depth:%d", p.PoolDepth())
-	return conn, err
-}
-
-func (p *ASConnectionPool) Put(c interface{}) {
+func (p *ASConnectionPool) PoolPut(c interface{}) {
 	err := p.connPool.Put(c)
 	if err != nil {
 		logger.Errorf("connection pool put")
 		return
 	}
-	//logger.Debugf("connection put, pool depth:%d", p.PoolDepth())
+}
+
+func (p *ASConnectionPool) PoolGet() (interface{}, error) {
+	conn, err := p.connPool.Get()
+	if err != nil {
+		dtmimp.E2P(err)
+	}
+	return conn, err
+}
+
+func (p *ASConnectionPool) Get() (interface{}, error) {
+	var wg sync.WaitGroup
+
+	clientChannel := make(chan *as.Client, 1)
+	errorChannel := make(chan error, 1)
+
+	wg.Add(1)
+	go func(wg *sync.WaitGroup, c chan *as.Client, e chan error) {
+		defer wg.Done()
+		seedserver := "10.211.55.200"
+		policy := as.NewClientPolicy()
+		policy.MinConnectionsPerNode = 5
+		//policy.User = config.User
+		//policy.Password = config.Password
+		policy.Timeout = time.Duration(300 * time.Second)
+
+		client, err := as.NewClientWithPolicy(policy, seedserver, 3000)
+		if err != nil {
+			c <- nil
+			e <- err
+			return
+		}
+		c <- client
+		e <- nil
+		return
+	}(&wg, clientChannel, errorChannel)
+
+	wg.Wait()
+	client := <-clientChannel
+	err := <-errorChannel
+	return client, err
+}
+
+func (p *ASConnectionPool) Put(c interface{}) {
+	c.(*as.Client).Close()
 }
 
 func (p *ASConnectionPool) Release() {
@@ -143,11 +157,3 @@ func (p *ASConnectionPool) Release() {
 func (p *ASConnectionPool) PoolDepth() int {
 	return p.connPool.Len()
 }
-
-//func (p *ASConnectionPool) GetUserName() string {
-//	return authVars.UserName
-//}
-
-//func (p *ASConnectionPool) GetPassword() string {
-//	return authVars.Password
-//}
